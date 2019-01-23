85c07cfcfa4d1521497d12525e7825c185ce6a0c
RecursiveHook <6758654+RecursiveHook@users.noreply.github.com>
2019-01-22 22:20:44 -0800

    Refactor AutoRoles

--- cogs/autorole.py
+++ cogs/autorole.py
@@ -1,6 +1,60 @@
 import discord
 from discord.ext import commands
 
+roles_dicts = {
+    'game_modes': {
+        'crucible': ['c', 'crucible'],
+        'gambit': ['g', 'gambit'],
+        'raid': ['r', 'raid'],
+        'strike-nf-pve': ['s', 'nf', 'pve', 'strike', 'nightfall', 'strike-nf-pve']
+    },
+    'raid_leads': {
+        'active': ['on', 'active', 'true', 'enable', 'yes', '1'],
+        'inactive': ['off', 'inactive', 'false', 'disable', 'no', '0']
+    }
+}
+
+
+async def update_roles(ctx,
+                       role_dict: dict,
+                       update_message: str = 'set',
+                       action: str = 'add',
+                       *roles: str
+                       ):
+
+    # Process input and sanitize
+    roles_to_update = set()
+    for r in roles:
+        for role, allowed_names in role_dict.items():
+            for name in allowed_names:
+                if r == name:
+                    roles_to_update.add(role)
+                    break
+
+    print(f'Roles to Update: {roles_to_update}')
+    print(f'Role Dict: {role_dict}')
+
+    if not list(roles_to_update):
+        return
+
+    # Build list of roles to add
+    updated_roles = [discord.utils.get(ctx.guild.roles, name=role) for role in roles_to_update]
+
+    # Add check if roles are already applied and avoid doing it again
+
+    print(f'"Updating Roles (action: {action}): {updated_roles}')
+    user = ctx.message.author
+
+    if action == 'add':
+        await user.add_roles(*updated_roles)
+    elif action == 'remove':
+        await user.remove_roles(*updated_roles)
+    else:
+        print(f"Unknown Action for Update Roles!")
+        return
+
+    await ctx.send(f'{ctx.message.author.mention} {update_message} role(s):  `{", ".join(roles_to_update)}`')
+
 
 class AutoRole:
     def __init__(self, bot):
@@ -8,7 +62,7 @@ class AutoRole:
 
     @commands.command(name='lfg-add')  # , aliases=['game-role', 'lfg-role'])
     @commands.guild_only()
-    async def add_roles(self, ctx, *roles: str):  # roles: list):
+    async def lfg_add(self, ctx, *roles: str):
         """Adds Game Mode roles for @ pings.
 
         Uses either short names like 'c' for crucible, or full names like 'gambit'.
@@ -21,6 +75,7 @@ class AutoRole:
         # $lfg all -- adds/removes all roles
         # Handle ALL Eventually...
 
+        # TODO: CLEAN UP
         # Build roles
         role_list = [
             'crucible',
@@ -28,45 +83,13 @@ class AutoRole:
             'raid',
             'strike-nf-pve'
         ]
-
         # role_dict = {role_name: [] for role_name in role_list}
-        role_dict = {
-            'crucible': ['c', 'crucible'],
-            'gambit': ['g', 'gambit'],
-            'raid': ['r', 'raid'],
-            'strike-nf-pve': ['s', 'nf', 'pve', 'strike', 'nightfall', 'strike-nf-pve']
-        }
 
-        # Process input and sanitize
-        roles_to_add = set()
-
-        for r in roles:
-            for role, allowed_names in role_dict.items():
-                for name in allowed_names:
-                    if r == name:
-                        roles_to_add.add(role)
-                        break
-
-        print(f'Roles to Add: {roles_to_add}')
-        print(f'Role Dict: {role_dict}')
-
-        if not list(roles_to_add):
-            return
-
-        # Build list of roles to add
-        new_roles = [discord.utils.get(ctx.guild.roles, name=role) for role in roles_to_add]
-
-        # Add check if roles are already applied and avoid doing it again
-
-        print(f'New Roles: {new_roles}')
-        #role = discord.utils.get(ctx.guild.roles, name="role to add name")
-        user = ctx.message.author
-        await user.add_roles(*new_roles)
-        await ctx.send(f'{ctx.message.author.mention} added Game Mode role(s):  `{", ".join(roles_to_add)}`')
+        await update_roles(ctx, roles_dicts['game_modes'], 'added Game Mode', action='add', *roles)
 
     @commands.command(name='lfg-remove')  # , aliases=['game-role', 'lfg-role'])
     @commands.guild_only()
-    async def remove_roles(self, ctx, *roles: str):  # roles: list):
+    async def lfg_remove(self, ctx, *roles: str):
         """Removes Game Mode roles for @ pings.
 
         Uses either short names like 'c' for crucible, or full names like 'gambit'.
@@ -74,53 +97,7 @@ class AutoRole:
         Multiple roles can be removed at once, e.g. `$lfg-remove c g` removes @crucible and @gambit.
         """
 
-        # $lfg (no param) -- Lists current LFG roles set
-        # $lfg role1 role2 -- adds/removes the roles
-        # $lfg all -- adds/removes all roles
-        # Handle ALL Eventually...
-
-        # Build roles
-        role_list = [
-            'crucible',
-            'gambit',
-            'raid',
-            'strike-nf-pve'
-        ]
-
-        # role_dict = {role_name: [] for role_name in role_list}
-        role_dict = {
-            'crucible': ['c', 'crucible'],
-            'gambit': ['g', 'gambit'],
-            'raid': ['r', 'raid'],
-            'strike-nf-pve': ['s', 'nf', 'pve', 'strike', 'nightfall', 'strike-nf-pve']
-        }
-
-        # Process input and sanitize
-        roles_to_add = set()
-
-        for r in roles:
-            for role, allowed_names in role_dict.items():
-                for name in allowed_names:
-                    if r == name:
-                        roles_to_add.add(role)
-                        break
-
-        print(f'Roles to Add: {roles_to_add}')
-        print(f'Role Dict: {role_dict}')
-
-        if not list(roles_to_add):
-            return
-
-        # Build list of roles to add
-        new_roles = [discord.utils.get(ctx.guild.roles, name=role) for role in roles_to_add]
-
-        # Add check if roles are already applied and avoid doing it again
-
-        print(f'New Roles: {new_roles}')
-        #role = discord.utils.get(ctx.guild.roles, name="role to add name")
-        user = ctx.message.author
-        await user.remove_roles(*new_roles)
-        await ctx.send(f'{ctx.message.author.mention} removed Game Mode role(s):  `{", ".join(roles_to_add)}`')
+        await update_roles(ctx, roles_dicts['game_modes'], 'removed Game Mode', action='remove', *roles)
 
 
 def setup(bot):
