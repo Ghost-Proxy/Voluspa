be1c66ca9e6ae295cfe911b1817bfda0a234a54d
RecursiveHook <6758654+RecursiveHook@users.noreply.github.com>
2019-05-19 01:06:05 -0700

    Cleanup and extend custom_embed

--- modules/custom_embed.py
+++ modules/custom_embed.py
@@ -4,15 +4,16 @@ from discord import Embed
 from voluspa import CONFIG
 
 
-def default_embed(title='Völuspá', description=''):
+def default_embed(title='Völuspá', description='', color=0x009933, **kwargs):
     embed = Embed(
         title=title,
         description=description,
-        color=0x009933
+        color=color,
+        **kwargs
     )
     # embed.set_author(name='\uFEFF', icon_url=f'{CONFIG.Resources.image_bucket_root_url}/ghost-proxy/GP_Logo-2.png')
     embed.set_footer(
-        text=f'via Völuspá with \u2764',
+        text='via Völuspá with \u2764',
         icon_url=f"{CONFIG.Resources.image_bucket_root_url}/voluspa/Voluspa_icon_64x48.png"
     )
     return embed
