bdf6ae668698b471db91d246314193a2e6fdc7b3
Harry Smith <harry.smith.0119@gmail.com>
2019-09-29 01:02:26 +1200

    Removed semicolons

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -257,10 +257,10 @@ class Utilities(commands.Cog):
                     if ctx.invoked_with in ['cd', 'collate-dark']:
                         line_colors = "#FFFFFF"
                         
-                        axes.spines['bottom'].set_color(line_colors);
-                        axes.spines['top'].set_color(line_colors);
-                        axes.spines['left'].set_color(line_colors);
-                        axes.spines['right'].set_color(line_colors);
+                        axes.spines['bottom'].set_color(line_colors)
+                        axes.spines['top'].set_color(line_colors)
+                        axes.spines['left'].set_color(line_colors)
+                        axes.spines['right'].set_color(line_colors)
                         
                         axes.tick_params(colors=line_colors)
                         
@@ -300,7 +300,7 @@ class Utilities(commands.Cog):
                     await ctx.send(file=discord.File(png_wrapper, filename=filename))
                     
                     png_wrapper.close()
-                    plt.close();
+                    plt.close()
                 except KeyError:
                     await ctx.send(f'Uh oh, I was unable to collate poll `{id}`. Sorry!')
 
