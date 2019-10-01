d43a852d2b478f36d6b1165ba958f9e2a60955aa
RecursiveHook <6758654+RecursiveHook@users.noreply.github.com>
2019-10-01 00:32:16 -0700

    Bump to Voluspa v0.0.8

--- voluspa.py
+++ voluspa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """Voluspa Ghost Proxy Discord Bot"""
-VOLUSPA_VERSION = 'v0.0.7a'
+VOLUSPA_VERSION = 'v0.0.8'
 # Bot Example: https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be
 
 import datetime
