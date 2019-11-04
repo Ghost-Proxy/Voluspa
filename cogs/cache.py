3ce221670655eac316d7ed53136b1e6a0eb57431
RecursiveHook <6758654+RecursiveHook@users.noreply.github.com>
2019-11-04 01:29:56 -0800

    Added cache-delete command, misc tweaks

--- cogs/cache.py
+++ cogs/cache.py
@@ -1,6 +1,6 @@
 import logging
 
-from modules.cache import CACHE_NAME, write_cache, read_cache
+import modules.cache as cache
 from modules.custom_embed import default_embed
 
 from discord.ext import commands
@@ -13,15 +13,17 @@ class Cache(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # TODO: This needs some refactoring/DRY'ing
+
     @commands.command(name='cache-write', aliases=['cw', 'ca', 'cache-add'])
     @commands.has_any_role('founder', 'ghost-proxy-vanguard', 'ghost-proxy-gatekeeper')
     async def cache_write(self, ctx, cache_key: str, *, cache_value: str):
         """Writes key/value to cache"""
-        cache_name = CACHE_NAME
+        cache_name = cache.CACHE_NAME
         if 'cache_name=' in cache_key:
             cache_name = cache_key.split('cache_name=')[1]
             cache_key, cache_value = cache_value.split(' ')
-        await write_cache(cache_key, cache_value, cache_name=cache_name)
+        await cache.write_cache(cache_key, cache_value, cache_name=cache_name)
         embed = default_embed(
             title='Cache Write (K/V)',
             description=f'Wrote the following Key/Value to "{cache_name}" cache'
@@ -37,11 +39,11 @@ class Cache(commands.Cog):
     @commands.has_any_role('founder', 'ghost-proxy-vanguard', 'ghost-proxy-gatekeeper')
     async def cache_read(self, ctx, *, cache_key: str):
         """Reads value for key from cache"""
-        cache_name = CACHE_NAME
+        cache_name = cache.CACHE_NAME
         if 'cache_name=' in cache_key:
             cache_name, cache_key = cache_key.split(' ')
             cache_name = cache_name.split('cache_name=')[1]
-        cache_value = await read_cache(cache_key, cache_name=cache_name)
+        cache_value = await cache.read_cache(cache_key, cache_name=cache_name)
         embed = default_embed(
             title='Cache Read (K/V)',
             description=f'Read the following Key/Value from "{cache_name}" cache'
@@ -49,6 +51,26 @@ class Cache(commands.Cog):
         embed.add_field(
             name=f'{cache_key}',
             value=f'{cache_value}',
+            inline=False
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command(name='cache-delete', aliases=['cd', 'cr', 'cache-remove'])
+    @commands.has_any_role('founder', 'ghost-proxy-vanguard', 'ghost-proxy-gatekeeper')
+    async def cache_delete(self, ctx, *, cache_key: str):
+        """Reads value for key from cache"""
+        cache_name = cache.CACHE_NAME
+        if 'cache_name=' in cache_key:
+            cache_name, cache_key = cache_key.split(' ')
+            cache_name = cache_name.split('cache_name=')[1]
+        cache_value = await cache.delete_cache(cache_key, cache_name=cache_name)
+        embed = default_embed(
+            title='Cache Delete (K/V)',
+            description=f'Deleted the following Key from "{cache_name}" cache'
+        )
+        embed.add_field(
+            name=f'{cache_key}',
+            value=f'_deleted_',
             inline=False
         )
         await ctx.send(embed=embed)
