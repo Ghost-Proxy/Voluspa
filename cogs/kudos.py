cbcb2426b2af3c5fb11b8104faadee91a70d8c61
RecursiveHook <6758654+RecursiveHook@users.noreply.github.com>
2019-11-22 22:59:14 -0800

    Added initial Kudos cogs framework with subcommands

---
+++ cogs/kudos.py
@@ -0,0 +1,36 @@
+import logging
+
+import modules.cache as cache
+
+import discord
+from discord.ext import commands
+
+logger = logging.getLogger('voluspa.cog.kudos')
+
+
+class Kudos(commands.Cog):
+    """Test of Caches"""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(aliases=['k', 'kudo'])
+    async def kudos(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invalid _kudos_ command.')
+
+    @kudos.command(name='view-player', aliases=['view player', 'vp'])
+    async def view_player_kudos(self, ctx):
+        await ctx.send('view_player_kudos')
+
+    @kudos.command(name='view-leaderboard', aliases=['view leaderboard', 'vl'])
+    async def view_kudos_leaderboard(self, ctx):
+        await ctx.send('view_kudos_leaderboard')
+
+    @kudos.command(name='send', aliases=['s'])
+    async def send_kudos(self, ctx):
+        await ctx.send('send_kudos')
+
+
+def setup(bot):
+    bot.add_cog(Kudos(bot))
+
