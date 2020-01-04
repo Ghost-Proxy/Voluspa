f85150657bd7b8db24f0afd666250dbc6be255d1
RecursiveHook <6758654+RecursiveHook@users.noreply.github.com>
2020-01-04 12:34:58 -0800

    Tweak cog commands due to discordpy restrictions

--- cogs/cog_control.py
+++ cogs/cog_control.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 
 # From: https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be#file-owner-py
 
+
 class CogControl(commands.Cog):
     # TODO: Use sub-command pattern... :D
     def __init__(self, bot):
@@ -10,7 +11,7 @@ class CogControl(commands.Cog):
     # Hidden means it won't show up on the default help.
     @commands.command(name='load', hidden=True)
     @commands.is_owner()
-    async def cog_load(self, ctx, *, cog: str):
+    async def load(self, ctx, *, cog: str):
         """Command which Loads a Module.
         Remember to use dot path. e.g: cogs.owner"""
 
@@ -23,7 +24,7 @@ class CogControl(commands.Cog):
 
     @commands.command(name='unload', hidden=True)
     @commands.is_owner()
-    async def cog_unload(self, ctx, *, cog: str):
+    async def unload(self, ctx, *, cog: str):
         """Command which Unloads a Module.
         Remember to use dot path. e.g: cogs.owner"""
 
@@ -36,7 +37,7 @@ class CogControl(commands.Cog):
 
     @commands.command(name='reload', hidden=True)
     @commands.is_owner()
-    async def cog_reload(self, ctx, *, cog: str):
+    async def reload(self, ctx, *, cog: str):
         """Command which Reloads a Module.
         Remember to use dot path. e.g: cogs.owner"""
 
